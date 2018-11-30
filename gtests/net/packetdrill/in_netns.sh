8f0733fef2c329a24d7527fba8cc36208be1be94
Willem de Bruijn <willemb@google.com>
2018-11-29 22:22:16 -0500

    net-test: packetdrill: add run_all.py script
    Add a script to run all tests under a directory:
    
        ./packetdrill/run_all.py tcp
    
        Ran   15 tests:   15 passing,    0 failing,    0 timed out (6.03 sec): tcp/
    
    Tests run in parallel. Optionally pass -S to serialize.
    Optionally report separate results for each subdirectory.
    
        ./packetdrill/run_all.py -s tcp
    
        Ran    0 tests:    0 passing,    0 failing,    0 timed out (0.00 sec): tcp/common
        Ran   15 tests:   15 passing,    0 failing,    0 timed out (6.03 sec): tcp/zerocopy
    
    To debug errors, pass -v and optionally -l for logging.
    
    To demonstrate, also import initial scripts in directory
    
      tcp/zerocopy
    
    and helpers in
    
      common
      tcp/common
    
    Import in_netns.sh from the Linux kernel sources
    (also GPLv2, and I am the author)

---
+++ gtests/net/packetdrill/in_netns.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Execute a subprocess in a network namespace
+
+set -e
+
+readonly NETNS="ns-$(mktemp -u XXXXXX)"
+
+setup() {
+	ip netns add "${NETNS}"
+	ip -netns "${NETNS}" link set lo up
+}
+
+cleanup() {
+	ip netns del "${NETNS}"
+}
+
+trap cleanup EXIT
+setup
+
+ip netns exec "${NETNS}" "$@"
+exit "$?"
