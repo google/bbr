37fed1f59b1440332ede3fc1fbd37d8072dd1906
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2021-06-28 11:07:22 +0300

    sched: Add dualpi2 qdisc
    DualPI2 provides L4S-type low latency & loss to traffic that uses a
    scalable congestion controller (e.g. TCP-Prague, DCTCP) without
    degrading the performance of 'classic' traffic (e.g. Reno,
    Cubic etc.). It is intended to be the reference implementation of the
    IETF's DualQ Coupled AQM.
    
    The qdisc provides two queues called low latency and classic. It
    classifies packets based on the ECN field in the IP headers. By
    default it directs non-ECN and ECT(0) into the classic queue and
    ECT(1) and CE into the low latency queue, as per the IETF spec.
    
    Each queue runs its own AQM:
    * The classic AQM is called PI2, which is similar to the PIE AQM but
      more responsive and simpler. Classic traffic requires a decent
      target queue (default 15ms for Internet deployment) to fully
      utilize the link and to avoid high drop rates.
    * The low latency AQM is, by default, a very shallow ECN marking
      threshold (1ms) similar to that used for DCTCP.
    
    The DualQ isolates the low queuing delay of the Low Latency queue
    from the larger delay of the 'Classic' queue. However, from a
    bandwidth perspective, flows in either queue will share out the link
    capacity as if there was just a single queue. This bandwidth pooling
    effect is achieved by coupling together the drop and ECN-marking
    probabilities of the two AQMs.
    
    The PI2 AQM has two main parameters in addition to its target delay.
    All the defaults are suitable for any Internet setting, but it can
    be reconfigured for a Data Centre setting. The integral gain factor
    alpha is used to slowly correct any persistent standing queue error
    from the target delay, while the proportional gain factor beta is
    used to quickly compensate for queue changes (growth or shrinkage).
    Either alpha and beta are given as a parameter, or they can be
    calculated by tc from alternative typical and maximum RTT parameters.
    
    Internally, the output of a linear Proportional Integral (PI)
    controller is used for both queues. This output is squared to
    calculate the drop or ECN-marking probability of the classic queue.
    This counterbalances the square-root rate equation of Reno/Cubic,
    which is the trick that balances flow rates across the queues. For
    the ECN-marking probability of the low latency queue, the output of
    the base AQM is multiplied by a coupling factor. This determines the
    balance between the flow rates in each queue. The default setting
    makes the flow rates roughly equal, which should be generally
    applicable.
    
    If DUALPI2 AQM has detected overload (due to excessive non-responsive
    traffic in either queue), it will switch to signaling congestion
    solely using drop, irrespective of the ECN field. Alternatively, it
    can be configured to limit the drop probability and let the queue
    grow and eventually overflow (like tail-drop).
    
    Additional details can be found in the draft:
      https://datatracker.ietf.org/doc/draft-ietf-tsvwg-aqm-dualq-coupled/
    
    Signed-off-by: Olga Albisser <olga@albisser.org>
    Signed-off-by: Koen De Schepper <koen.de_schepper@nokia-bell-labs.com>
    Signed-off-by: Olivier Tilmans <olivier.tilmans@nokia-bell-labs.com>
    Signed-off-by: Bob Briscoe <research@bobbriscoe.net>
    Signed-off-by: Henrik Steen <henrist@henrist.net>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>

--- include/uapi/linux/pkt_sched.h
+++ include/uapi/linux/pkt_sched.h
@@ -1262,4 +1262,38 @@ enum {
 
 #define TCA_ETS_MAX (__TCA_ETS_MAX - 1)
 
+/* DUALPI2 */
+enum {
+	TCA_DUALPI2_UNSPEC,
+	TCA_DUALPI2_LIMIT,		/* Packets */
+	TCA_DUALPI2_TARGET,		/* us */
+	TCA_DUALPI2_TUPDATE,		/* us */
+	TCA_DUALPI2_ALPHA,		/* Hz scaled up by 256 */
+	TCA_DUALPI2_BETA,		/* HZ scaled up by 256 */
+	TCA_DUALPI2_STEP_THRESH,	/* Packets or us */
+	TCA_DUALPI2_STEP_PACKETS,	/* Whether STEP_THRESH is in packets */
+	TCA_DUALPI2_COUPLING,		/* Coupling factor between queues */
+	TCA_DUALPI2_DROP_OVERLOAD,	/* Whether to drop on overload */
+	TCA_DUALPI2_DROP_EARLY,		/* Whether to drop on enqueue */
+	TCA_DUALPI2_C_PROTECTION,	/* Percentage */
+	TCA_DUALPI2_ECN_MASK,		/* L4S queue classification mask */
+	TCA_DUALPI2_SPLIT_GSO,		/* Split GSO packets at enqueue */
+	TCA_DUALPI2_PAD,
+	__TCA_DUALPI2_MAX
+};
+
+#define TCA_DUALPI2_MAX   (__TCA_DUALPI2_MAX - 1)
+
+struct tc_dualpi2_xstats {
+	__u32 prob;		/* current probability */
+	__u32 delay_c;		/* current delay in C queue */
+	__u32 delay_l;		/* current delay in L queue */
+	__s32 credit;		/* current c_protection credit */
+	__u32 packets_in_c;	/* number of packets enqueued in C queue */
+	__u32 packets_in_l;	/* number of packets enqueued in L queue */
+	__u32 maxq;		/* maximum queue size */
+	__u32 ecn_mark;		/* packets marked with ecn*/
+	__u32 step_marks;	/* ECN marks due to the step AQM */
+};
+
 #endif
