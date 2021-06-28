6c64d6ea9f11a6f0926511975f732fe64fe38c98
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2021-06-28 11:07:20 +0300

    Add ece_delta to rate_sample
    Include echoed CE count into rate_sample. Replace local ecn_count
    variable with it.
    
    Signed-off-by: Olivier Tilmans <olivier.tilmans@nokia-bell-labs.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Change-Id: I5a29d2ea7617a6644cbac508aafe403a5549e08a

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1135,6 +1135,7 @@ struct rate_sample {
 	int  losses;		/* number of packets marked lost upon ACK */
 	u32  acked_sacked;	/* number of packets newly (S)ACKed upon ACK */
 	u32  prior_in_flight;	/* in flight before this ACK */
+	u32  ece_delta;		/* is this ACK echoing some received CE? */
 	u32  last_end_seq;	/* end_seq of most recently ACKed packet */
 	bool is_app_limited;	/* is sample from packet with bubble in pipe? */
 	bool is_retrans;	/* is sample from retransmission? */
