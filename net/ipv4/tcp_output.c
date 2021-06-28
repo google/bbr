b48b8c7b75d374f221f569ad8b8401203a6cf762
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2021-06-28 11:07:20 +0300

    tcp: L4S ECT(1) identifier for CC modules
    When ECN is successfully negociated for a TCP flow, it defaults to
    always use ECT(0) in the IP header. L4S service, however, needs to
    use ECT(1).
    
    This patch enables congestion control algorithms to control whether
    ECT(0) or ECT(1) should be used on a per-segment basis. A new
    CA module flag (TCP_CONG_WANTS_ECT_1) defines the behavior
    expected by the CA when not-yet initialized for the connection.
    As such, it implicitely assumes that the CA also has the
    TCP_CONG_NEEDS_ECN set.
    
    Signed-off-by: Olivier Tilmans <olivier.tilmans@nokia-bell-labs.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
    Signed-off-by: Neal Cardwell <ncardwell@google.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -313,7 +313,7 @@ static void tcp_ecn_send_synack(struct sock *sk, struct sk_buff *skb)
 		TCP_SKB_CB(skb)->tcp_flags &= ~TCPHDR_ECE;
 	else if (tcp_ca_needs_ecn(sk) ||
 		 tcp_bpf_ca_needs_ecn(sk))
-		INET_ECN_xmit(sk);
+		__INET_ECN_xmit(sk, tcp_ca_wants_ect_1(sk));
 
 	if (tp->ecn_flags & TCP_ECN_MODE_ACCECN) {
 		TCP_SKB_CB(skb)->tcp_flags &= ~TCPHDR_ACE;
@@ -347,7 +347,7 @@ static void tcp_ecn_send_syn(struct sock *sk, struct sk_buff *skb)
 
 	if (use_ecn) {
 		if (tcp_ca_needs_ecn(sk) || bpf_needs_ecn)
-			INET_ECN_xmit(sk);
+			__INET_ECN_xmit(sk, tcp_ca_wants_ect_1(sk));
 
 		TCP_SKB_CB(skb)->tcp_flags |= TCPHDR_ECE | TCPHDR_CWR;
 		if (use_accecn) {
@@ -416,7 +416,8 @@ static void tcp_ecn_send(struct sock *sk, struct sk_buff *skb,
 		return;
 
 	if (!tp->ecn_fail)
-		INET_ECN_xmit(sk);
+		/* The CCA could change the ECT codepoint on the fly, reset it*/
+		__INET_ECN_xmit(sk, tp->ecn_flags & TCP_ECN_ECT_1);
 	if (tcp_ecn_mode_accecn(tp)) {
 		tcp_accecn_set_ace(tp, skb, th);
 		skb_shinfo(skb)->gso_type |= SKB_GSO_TCP_ACCECN;
