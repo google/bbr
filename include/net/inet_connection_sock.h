99211b80677c5e26e22f17345dcec8c7cd3c8019
Neal Cardwell <ncardwell@google.com>
2021-06-28 11:07:23 +0300

    tcp_bbr2: TCP BBRv2 alpha
    BBR v2 from https://github.com/google/bbr.git v2alpha branch,
    with recent fixes/updates from Google's internal BBRv2 code.
    
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    
    Change-Id: I48b1c996f7d27952c28feda1770108508ede88e9

--- include/net/inet_connection_sock.h
+++ include/net/inet_connection_sock.h
@@ -134,7 +134,7 @@ struct inet_connection_sock {
 	u32			  icsk_probes_tstamp;
 	u32			  icsk_user_timeout;
 
-	u64			  icsk_ca_priv[104 / sizeof(u64)];
+	u64			  icsk_ca_priv[216 / sizeof(u64)];
 #define ICSK_CA_PRIV_SIZE	  sizeof_field(struct inet_connection_sock, icsk_ca_priv)
 };
 
