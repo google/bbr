c530ce79152af0c6f6780c770769bf3d75ddf447
Neal Cardwell <ncardwell@google.com>
2022-08-14 15:52:25 -0400

    fixup "tcp: AccECN option failure handling" ::: fix zeroing check in tcp_accecn_option_init()
    <one line summary>
    
    <multiple>
    <lines>
    <description>
    
    Tested: <multi line explanation of how the code was tested>
    
    Effort: <Your effort group>
    Google-Bug-Id: <buganizer-id>
    Change-Id: Iaa41f4925e5847960c32f4b78cb65ee34be0fa26

--- net/ipv4/tcp_minisocks.c
+++ net/ipv4/tcp_minisocks.c
@@ -496,28 +496,24 @@ u8 tcp_accecn_option_init(const struct sk_buff *skb, u8 opt_offset)
 {
 	unsigned char *ptr = skb_transport_header(skb) + opt_offset;
 	unsigned int optlen = ptr[1] - 2;
-	bool order1;
 
 	WARN_ON_ONCE(ptr[0] != TCPOPT_ACCECN0 && ptr[0] != TCPOPT_ACCECN1);
-	order1 = (ptr[0] == TCPOPT_ACCECN1);
 	ptr += 2;
 
-	if (optlen >= TCPOLEN_ACCECN_PERFIELD) {
-		if (order1) {
-			if (optlen < TCPOLEN_ACCECN_PERFIELD * 3)
-				return TCP_ACCECN_OPT_COUNTER_SEEN;
-			ptr += TCPOLEN_ACCECN_PERFIELD * 2;
-		}
-		/* Detect option zeroing. Check the e0b counter value,
-		 * if present, it must be != 0.
-		 */
-		if (!(get_unaligned_be32(ptr - 1) & 0xFFFFFFU))
-			return TCP_ACCECN_OPT_FAIL;
-
+	/* Detect option zeroing: an AccECN connection "MAY check that the
+	 * initial value of the EE0B field or the EE1B field is non-zero"
+	 */
+	if (optlen < TCPOLEN_ACCECN_PERFIELD)
+		return TCP_ACCECN_OPT_EMPTY_SEEN;
+	if (get_unaligned_be24(ptr) == 0)
+		return TCP_ACCECN_OPT_FAIL;
+	if (optlen < TCPOLEN_ACCECN_PERFIELD * 3)
 		return TCP_ACCECN_OPT_COUNTER_SEEN;
-	}
+	ptr += TCPOLEN_ACCECN_PERFIELD * 2;
+	if (get_unaligned_be24(ptr) == 0)
+		return TCP_ACCECN_OPT_FAIL;
 
-	return TCP_ACCECN_OPT_EMPTY_SEEN;
+	return TCP_ACCECN_OPT_COUNTER_SEEN;
 }
 
 /* This is not only more efficient than what we used to do, it eliminates
